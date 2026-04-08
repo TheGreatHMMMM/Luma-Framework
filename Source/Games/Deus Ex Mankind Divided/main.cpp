c8543f21fbd6ea5612b4ecb672505d0d0766ec20
garamond13 <98652255+garamond13@users.noreply.github.com>
2026-04-08 18:07:46 +0200

    Luma: Add Deus Ex: Mankind Divided project

---
+++ Source/Games/Deus Ex Mankind Divided/main.cpp
@@ -0,0 +1,58 @@
+#define GAME_DEUS_EX_MANKIND_DIVIDED 1
+
+#include "..\..\Core\core.hpp"
+
+class GameDeusExMankindDivided final : public Game
+{
+public:
+
+    void OnInit(bool async) override
+    {
+        // ### Update these (find the right values) ###
+        // ### See the "GameCBuffers.hlsl" in the shader directory to expand settings ###
+        luma_settings_cbuffer_index = 8;
+        luma_data_cbuffer_index = -1;
+    }
+
+    void PrintImGuiAbout() override
+    {
+        ImGui::Text("Deus Ex: Mankind Divided Luma mod - about and credits section", "");
+    }
+};
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+        Globals::SetGlobals(PROJECT_NAME, "Deus Ex: Mankind Divided Luma mod");
+        Globals::VERSION = 1;
+
+        swapchain_format_upgrade_type  = TextureFormatUpgradesType::AllowedEnabled;
+        swapchain_upgrade_type         = SwapchainUpgradeType::scRGB;
+        texture_format_upgrades_type   = TextureFormatUpgradesType::AllowedEnabled;
+        // ### Check which of these are needed and remove the rest ###
+        texture_upgrade_formats = {
+            reshade::api::format::r8g8b8a8_unorm,
+            reshade::api::format::r8g8b8a8_unorm_srgb,
+            reshade::api::format::r8g8b8a8_typeless,
+            reshade::api::format::r8g8b8x8_unorm,
+            reshade::api::format::r8g8b8x8_unorm_srgb,
+            reshade::api::format::b8g8r8a8_unorm,
+            reshade::api::format::b8g8r8a8_unorm_srgb,
+            reshade::api::format::b8g8r8a8_typeless,
+            reshade::api::format::b8g8r8x8_unorm,
+            reshade::api::format::b8g8r8x8_unorm_srgb,
+            reshade::api::format::b8g8r8x8_typeless,
+
+            reshade::api::format::r11g11b10_float,
+        };
+        // ### Check these if textures are not upgraded ###
+        texture_format_upgrades_2d_size_filters = 0 | (uint32_t)TextureFormatUpgrades2DSizeFilters::SwapchainResolution | (uint32_t)TextureFormatUpgrades2DSizeFilters::SwapchainAspectRatio;
+
+        game = new GameDeusExMankindDivided();
+    }
+
+    CoreMain(hModule, ul_reason_for_call, lpReserved);
+
+    return TRUE;
+}
